e6147e2114fe2306a67e36e6c0afd4b303733c13
ScreaM <gambolanddarvin@mail.ru>
2023-04-14 22:47:15 +0300

    Code Wars 0.0.1

--- 7kyo/Reverse words/script.js
+++ 7kyo/Reverse words/script.js
@@ -2,4 +2,4 @@ function reverseWords(str) {
 	return str.split("").reverse().join("").split(" ").reverse().join(" ")
 }
 
-console.log(reverseWords('The quick brown fox jumps over the lazy dog.'))
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.')) // ehT kciuq nworb xof spmuj revo eht yzal .god
